c4ca6df0865a0f45d93d64c9c952cee6fd2e2643
Vinicius Brito <viniciusdeab@outlook.com>
2023-01-26 23:42:09 -0300

    dinamica com o hook use countdown para convertes os dias, horas, minutes e segundos

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,20 +1,25 @@
 import Title from './components/Title';
 import Counter from './components/Counter';
 
-import clock from './assets/clock.jpg'
+import clock from './assets/clock.jpg';
+
+import useCountdown from './hooks/userCountdown';
 
 import './App.css';
 
 function App() {
+  const[day, hour, minute, second] = useCountdown("Jan 1, 2024 00:00:00");
+
+
   return (
     <div className="App" style={{backgroundImage: `url(${clock})`}}>
         <div className="container">
-          <Title title ="Contagem regressiva para 2023" />
+          <Title title ="Contagem regressiva para 2024" />
           <div className="countdown-container">
-            <Counter title="Dias" number={2}/>
-            <Counter title="Horas" number={2}/>
-            <Counter title="Minutos" number={2}/>
-            <Counter title="Segundos" number={2}/>
+            <Counter title="Dias" number={day}/>
+            <Counter title="Horas" number={hour}/>
+            <Counter title="Minutos" number={minute}/>
+            <Counter title="Segundos" number={second}/>
           </div>
         </div>
     </div>
