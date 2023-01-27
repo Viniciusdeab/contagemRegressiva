a0ae079b11c1f857e3226304f1813b4d49ac991f
Vinicius Brito <viniciusdeab@outlook.com>
2023-01-26 21:56:03 -0300

    Criação do compomente Counter

---
+++ countdown/src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = ({ title, number }) => {
+    return (
+        <div className='counter'>
+            <p className='counter-number'>{number}</p>
+            <div className='counter-text'>{title}</div>
+        </div>
+    )
+}
+
+export default Counter
